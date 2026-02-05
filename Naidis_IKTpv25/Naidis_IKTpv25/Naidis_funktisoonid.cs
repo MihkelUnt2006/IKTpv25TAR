8a3774a4c87edb0bd16ea20b40b4f97d57c142b4
Opilane TTHK <opilane@tthk.ee>
2026-02-05 12:19:14 +0200

    commit

---
+++ Naidis_IKTpv25/Naidis_IKTpv25/Naidis_funktisoonid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class Naidis_funktisoonid
+    {
+        public static float Summa(float arv1, float arv2)
+        {
+            return arv1 + arv2;
+
+        }
+    }
+}
---
+++ Naidis_IKTpv25/Naidis_IKTpv25/StartPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Naidis_IKTpv25
+{
+    public class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("1. Osa Andmetüübid, Alamfunktsioonid/meetodid");
+            string tekst = "Tere tulemast C#-i maailma! ";
+            Console.WriteLine($"oli loodud muutuja tekst, mis võrdun: {tekst}");
+            Console.WriteLine("\"oli loodud muutuja tekst, mis võrdun: {0}", tekst);
+            Console.Write("Mis on sinu nimi");
+            string nimi = Console.ReadLine();
+            Console.Write("Kui vana sa oled?: ");
+            try
+            {
+               int vanus= int.Parse(Console.ReadLine());
+               Console.WriteLine($"Tere {nimi}. Sa oled {vanus} aastat vana");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                
+            }
+            try
+            {
+               Console.Write("Arv 1: ");
+               float a = float.Parse( Console.ReadLine());
+               Console.Write("arv 2: ");
+               float b = float.Parse(Console.ReadLine());
+                float vastus = Naidis_funktisoonid.Summa(a, b);
+                Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+           
+
+
+        }
+
+    }
+}
