a681ec5ee06c7bcae5a4d1a0d6250e53e3371d2d
Opilane TTHK <opilane@tthk.ee>
2026-03-11 15:00:42 +0200

    Tunnitöö

---
+++ ITaaliaToit/MainPage_classidega.cs
@@ -0,0 +1,10 @@
+﻿namespace ITaaliaToit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
