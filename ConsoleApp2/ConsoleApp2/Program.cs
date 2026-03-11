5a24583e8b37dd06f9fa9dee3150ed88754ad2e8
Mihkel <mihunt29@gmail.com>
2026-03-11 19:43:28 +0200

    c

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+static void ristkylik()
+{
+    Console.WriteLine("Sisesta arv A");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Siseta arv B");
+    double b = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Sisesta arv C");
+    double c = Convert.ToDouble(Console.ReadLine());
+
+    double D = b * b - 4 * a * c;
+
+    if (D > 0)
+    {
+        double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+        Console.WriteLine("Lahendid: x1 = " + x1 + ", x2 = " + x2);
+    }
+    else if (D == 0)
+    {
+        double x = -b / (2 * a);
+        Console.WriteLine("Üks lahend: x = " + x);
+    }
+    else
+    {
+        Console.WriteLine("Lahendeid pole");
+    }
+
+    Console.WriteLine("Hello, World!");
+}
--- Naidis_IKTpv25/Naidis_IKTpv25/StartPage.cs
+++ Naidis_IKTpv25/Naidis_IKTpv25/StartPage.cs
@@ -9,17 +9,17 @@ namespace Naidis_IKTpv25
     {
         public static void Main(string[] args)
         {
-            bool working = true;
-            while (working)
-            {
-                Console.WriteLine(" Peamine menüü ");
-                Console.WriteLine("1 Osa 1 funktsioonid");
-                Console.WriteLine("2 Osa 2 funktsioonid");
-                Console.WriteLine("3 Osa 3 funktsioonid");
-                Console.WriteLine("4 exit");
-                Console.Write("Vali osa");
-                int osa = Convert.ToInt32(Console.ReadLine());
-            }
+            //bool working = true;
+            //while (working)
+            //{
+            //    Console.WriteLine(" Peamine menüü ");
+            //    Console.WriteLine("1 Osa 1 funktsioonid");
+            //    Console.WriteLine("2 Osa 2 funktsioonid");
+            //    Console.WriteLine("3 Osa 3 funktsioonid");
+            //    Console.WriteLine("4 exit");
+            //    Console.Write("Vali osa");
+            //    int osa = Convert.ToInt32(Console.ReadLine());
+            //}
             {
 
             }
