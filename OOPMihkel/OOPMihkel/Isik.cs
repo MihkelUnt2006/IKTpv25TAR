8e9d709da8f958c40168189be4418e4a0eae838a
Opilane TTHK <opilane@tthk.ee>
2026-04-14 09:51:42 +0300

    OPP

---
+++ OOPMihkel/OOPMihkel/Isik.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPMihkel
+{
+    public class Isik
+    {
+        // Omadused ehk väljad
+        //public string Nimi;
+        //public int Vanus;
+
+        // Tegevus ehk meetod 
+        public void Tervita()
+        {
+            if(string.IsNullOrEmpty(Nimi) || sünniaasta == 0)
+            {
+                Console.WriteLine("Tere! Ma ei tea veel oma nime ega sünniaastat");
+            }
+            else
+            {
+                Console.WriteLine($"Tere! Mina olen {Nimi}, ma olen {Vanus} ja ma olen sündinud aastal {Sünniaasta}");
+            }
+                
+        }
+
+
+
+        // Privaatne väli - otse ligi ei saa
+        private int sünniaasta;
+
+            // Avalik omadus (Property) automaatse get/set logikaga
+            public string Nimi { get; set; }
+
+            // Kontrollitud omadus
+            public int Sünniaasta
+            {
+                get { return sünniaasta; }
+                set
+                {
+                    if (value > 1900 && value <= DateTime.Now.Year)
+                        sünniaasta = value;
+                    else
+                        Console.WriteLine("Vigane sünniaasta!");
+
+                    
+                }
+            }
+
+
+
+        // Arvutatud omadus (ainult lugemiseks / getter)
+        public int Vanus => sünniaasta == 0 ? 0 : DateTime.Now.Year - sünniaasta;
+        
+    }
+}
---
+++ OOPMihkel/OOPMihkel/StartPage.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPMihkel
+{
+    public class StartPage
+    {
+        static void Main(string[] args)
+        {
+            Isik inimene1 = new Isik();
+            inimene1.Nimi = "Mati";
+            inimene1.Sünniaasta = 2026;
+            inimene1.Tervita(); // Väljund: Tere! Mina olen Mati...
+        }
+    }
+}
